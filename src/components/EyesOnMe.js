8ab9dbbb5bd2489ed86d72c55c8cfa0d480df00c
Kyle-Neale <kyle.a.neale@gmail.com>
2018-09-18 11:31:35 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
 // Code EyesOnMe Component Here
+
+export default class EyesOnMe extends Component {
+
+  handleFocus = (event) => {
+    console.log("Good!")
+  }
+
+  handleBlur = (event) => {
+    console.log("Hey! Eyes on me!");
+  }
+
+  render() {
+    return (
+
+      <button
+      onFocus={this.handleFocus}
+      onBlur={this.handleBlur}
+      >
+      Hey!
+      </button>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
 // Code Keypad Component Here
+
+export default class Keypad extends Component {
+
+  handleKeyUp = () => {
+    console.log("Entering password...")
+  }
+
+  render() {
+    return (
+      <input
+      type="password"
+      placeholder="Enter Password"
+      onKeyUp={this.handleKeyUp}
+      >
+
+      </input>
+    )
+  }
+}
